81bdba2f2067b7f4d3b884b37b336e8a9ff14dfd
madfour <873912205@qq.com>
2021-03-12 00:15:26 +0800

    Upadte

--- src/layout/component/sidebar.vue
+++ src/layout/component/sidebar.vue
@@ -17,10 +17,8 @@
           <i class="el-icon-location"></i>
           <span>导航一</span>
         </template>
-        <el-menu-item-group>
-          <el-menu-item index="1-1">选项1</el-menu-item>
-          <el-menu-item index="1-2">选项2</el-menu-item>
-        </el-menu-item-group>
+        <el-menu-item index="1-1">选项1</el-menu-item>
+        <el-menu-item index="1-2">选项2</el-menu-item>
       </el-submenu>
       <el-menu-item index="2">
         <i class="el-icon-menu"></i>
